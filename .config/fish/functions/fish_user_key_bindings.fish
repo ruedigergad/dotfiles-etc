d5ffd6cd8b6f3f21525911ba6c58505ebf07141e
Ruediger Gad <r.c.g@gmx.de>
2015-07-17 21:35:18 +0200

    Add fish config.

---
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,7 @@
+bind \e, 'history-token-search-backward'
+bind \e. 'history-token-search-forward'
+bind \eup 'history-token-search-backward'
+bind \edown 'history-token-search-forward'
+bind \ebackspace 'backward-kill-word'
+bind \eleft 'backward-word'
+bind \eright 'forward-word'
